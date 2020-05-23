3659c173928f701f3026bbcbbce42e5d29fe6334
anrito <anrioboladze@gmail.com>
2020-05-23 21:00:10 +0400

    test file for palendrome

---
+++ test.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+function isPalendrome(str) {
+  let strReverce = "";
+  for (let index = str.length - 1; index >= 0; index--) {
+    strReverce += str[index];
+  }
+  if (strReverce === str) return true;
+  return false;
+}
+
+var longestPalindrome = function (s) {
+  if (isPalendrome(s)) return s;
+  let longeestPallendrome = "";
+  for (let i = 0; i < s.length; i++) {
+    for (let j = s.length - 1; j > i; j--) {
+      let pal = s.substr(i, j);
+      console.log(pal);
+      if (isPalendrome(pal)) {
+        if (pal.length > longeestPallendrome.length) longeestPallendrome = pal;
+      }
+    }
+  }
+  return longeestPallendrome;
+};
+
+console.log(longestPalindrome("abcd"));
+
+// console.log("abcd".substr(0, 2));
